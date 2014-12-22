f6cc0a36d040bcd45f884cc5945eaadf4fddeec1
charlesxu90 <charlesxu90@gmail.com>
2014-12-22 03:19:03 +0300

    CompareVersionNumbers.java  ExcelSheetColumnTitle.java added

---
+++ ExcelSheetColumnTitle.java
@@ -0,0 +1,24 @@
+/***
+ * Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+ *
+ * For example:
+ *
+ *     1 -> A
+ *     2 -> B
+ *     3 -> C
+ *     ...
+ *     26 -> Z
+ *     27 -> AA
+ *     28 -> AB 
+ */
+public class Solution {
+    public String convertToTitle(int n) {
+        StringBuffer ret = new StringBuffer();
+        while ( n != 0) {
+            ret.insert(0, (char)( (int)('A') + (n - 1) % 26));
+            n = (n - 1) / 26;
+        }
+
+        return ret.toString();
+    }
+}
