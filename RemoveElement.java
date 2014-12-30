03a7d609a7608b5df74ec59871a5d10ee2d4d342
charlesxu90 <charlesxu90@gmail.com>
2014-12-30 21:59:07 +0300

    add RemoveElement.java

--- RemoveElement.java
+++ RemoveElement.java
@@ -4,6 +4,7 @@
  * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
  */
 public class Solution {
+    /*
     public int removeElement(int[] A, int elem) {
         if (A.length == 0)
             return 0;
@@ -15,5 +16,14 @@ public class Solution {
                 A[i - j] = A[i];
         }
         return A.length - j;
+    }
+    */
+    public int removeElement(int[] A, int elem) {
+        int index = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] != elem)
+                A[index++] = A[i];
+        }
+        return index;
     }
 }
