f67bad932cf8aae801669aebad3008cd24fb907a
charlesxu90 <charlesxu90@gmail.com>
2015-01-03 02:37:02 +0300

    add PlusOne.java

--- PlusOne.java
+++ PlusOne.java
@@ -6,35 +6,24 @@
 public class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-        if (n == 0)
-            return null;
-        int addup = 0;
-        if (digits[n - 1] + addup + 1 >= 10) {
-            digits[n - 1] += addup + 1 - 10;
-            addup = 1;
-        } else {
-            digits[n - 1] += addup + 1;
-            addup = 0;
-        }
+        int addup = 1;
 
-        for (int i = n - 2; i >= 0; i--){
+        for (int i = n - 1; i >= 0; i--){
             if (digits[i] + addup >= 10) {
                 digits[i] += addup - 10;
-                addup = 1;
             } else {
                 digits[i] += addup;
                 addup = 0;
             }
+            if (addup == 0)
+                return digits;
         }
 
-        if (addup == 1) {
-            int newdigits[] = new int[n + 1];
-            newdigits[0] = 1;
-            for (int i = 2; i <= n; i++)
-                newdigits[i] = digits[i - 1];
-            return newdigits;
-        } else
-            return digits;
+        int newdigits[] = new int[n + 1];
+        newdigits[0] = 1;
+        for (int i = 2; i <= n; i++)
+            newdigits[i] = digits[i - 1];
+        return newdigits;
 
     }
 }
