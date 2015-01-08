7cc148d5a6596ed135588ef1ce1796680fefb042
charlesxu90 <charlesxu90@gmail.com>
2015-01-08 13:29:37 +0300

    add MinimumPathSum.java

--- MinimumPathSum.java
+++ MinimumPathSum.java
@@ -4,7 +4,7 @@
  * Note: You can only move either down or right at any point in time.
  */
 public class Solution {
-    // Solution 1: DP 
+    // Solution 1: DP O(m * n) O(m * n)
     // f[x][y] = min(f[x - 1][y], f[x][y - 1]) + gird[x][y], store path
     public int minPathSum(int[][] grid) {
         int m = grid.length;
@@ -20,5 +20,21 @@ public class Solution {
             for (int j = 1; j < n; j++)
                 f[i][j] = Math.min(f[i - 1][j], f[i][j - 1]) + grid[i][j];
         return f[m - 1][n - 1];
+    }
+
+    // Solution 2: DP + Array O(m*n) O(n)
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        if (m == 0) return 0;
+        int n = grid[0].length;
+        int f[] = new int[n];
+        Arrays.fill(f, Integer.MAX_VALUE);
+        f[0] = 0;
+        for (int i = 0; i < m; i++) {
+            f[0] += grid[i][0];
+            for (int j = 1; j < n; j++)
+                f[j] = min(f[j - 1], f[j]) + grid[i][j];
+        }
+        return f[n - 1];
     }
 }
